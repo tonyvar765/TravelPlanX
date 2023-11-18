474d630ae1dbdf61ed08c8d5cd259afcb6c2ebae
Tony <tonyvar666@gmail.com>
2023-11-18 12:18:08 +0530

    design user interface

---
+++ script.js
@@ -0,0 +1,13 @@
+// JavaScript for handling form submission and other interactions
+document.getElementById('itineraryForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const destination = document.getElementById('destinationInput').value;
+    const activity = document.getElementById('activityInput').value;
+    // Code to handle adding destinations/activities to the itinerary list
+  });
+  
+  document.getElementById('loginBtn').addEventListener('click', function() {
+    // Code to handle login functionality
+    // You might integrate with social media login APIs here
+  });
+  
